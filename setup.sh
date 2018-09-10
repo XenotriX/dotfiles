408fb649ea2f9eadb1933a8c5c5d50451fa2e715
Tibo Clausen <33636216+XenotriX1337@users.noreply.github.com>
2018-09-10 22:09:10 +0200

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,4 @@
+echo "Setting up dotfiles"
+echo "source $HOME/dotfiles/zsh/zshinit.sh" > ~/.zshrc
+echo "so $HOME/dotfiles/vim/vimrc.vim" > ~/.vimrc
+echo "Done"
---
+++ zsh/zshinit.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+export ZSH=$HOME/dotfiles/zsh/plugins/oh-my-zsh
+source ~/dotfiles/zsh/zshrc.sh
+source $ZSH/oh-my-zsh.sh
