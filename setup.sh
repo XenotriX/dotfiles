40eb9f6484f7258736a7b9a7609584a1c3747afc
Tibo Clausen <33636216+XenotriX1337@users.noreply.github.com>
2018-11-05 11:56:14 +0100

    Add git config

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 echo "Setting up dotfiles"
 echo "source $HOME/dotfiles/zsh/zshinit.sh" > ~/.zshrc
 echo "so $HOME/dotfiles/vim/setup.vim" > ~/.vimrc
+git config --global include.path "$HOME/dotfiles/git/.gitconfig"
 echo "Done"
