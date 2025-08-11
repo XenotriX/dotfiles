075eb05213bb6869a9cd00a3cf97ce6e0e22fa86
Tibo Clausen <tibo.clausen@gmail.com>
2025-08-11 20:59:23 +0200

    Update LSP config to use built-in config mechanism

--- nvim/.config/nvim/lua/xeno/lsp.lua
+++ nvim/.config/nvim/lua/xeno/lsp.lua
@@ -17,34 +17,6 @@ require("mason").setup({
 
 require('mason-lspconfig').setup()
 
--- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-capabilities.offsetEncoding = { 'utf-16' }
-
--- The following example advertise capabilities to `clangd`.
-require('lspconfig').clangd.setup {
-    -- capabilities = capabilities,
-    -- root_dir = function() return "/home/tibo/Dev/Work/nussbaum-gateway/sensor/" end,
-    cmd = {
-        'clangd',
-        '--background-index',
-        '--clang-tidy',
-        '--query-driver=/home/tibo/.espressif/tools/xtensa-esp*-elf/**/xtensa-esp*-elf/bin/xtensa-esp32*-elf-*',
-        '--experimental-modules-support',
-    },
-}
-
-require('lspconfig').ts_ls.setup {
-    capabilities = capabilities,
-}
-
-require('lspconfig')['lua_ls'].setup {
-    capabilities = capabilities,
-}
-
--- require('lspconfig')['pylsp'].setup {
---     capabilities = capabilities,
--- }
 local function extra_args()
     local virtual = os.getenv("VIRTUAL_ENV") or "/usr"
     return { "--python-executable", virtual .. "/bin/python3", true }
@@ -59,52 +31,15 @@ vim.lsp.config('pylsp', {
                     overrides = extra_args(),
                     report_progress = true,
                     live_mode = true,
-                }
+                },
+                pylsp_rope = { enabled = true },
+                pylsp_code_actions = { enabled = true },
+                python_lsp_ruff = { enabled = true, formatEnabled = true },
             }
         }
     }
 })
 
-
-require('lspconfig').dartls.setup {
-    capabilities = capabilities,
-}
-
-require('lspconfig').dockerls.setup {
-    capabilities = capabilities,
-}
-
-require('lspconfig').gdscript.setup {
-    capabilities = capabilities,
-}
-
-require('lspconfig').html.setup {
-    capabilities = capabilities,
-}
-
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-require('lspconfig').jsonls.setup {
-    capabilities = capabilities,
-}
-
-require('lspconfig').cssls.setup {
-    capabilities = capabilities,
-}
-
-require('lspconfig').rust_analyzer.setup {}
-
-require('lspconfig').slint_lsp.setup {}
-
--- local signs = {
---     { name = "DiagnosticSignError", text = "" },
---     { name = "DiagnosticSignWarn",  text = "" },
---     { name = "DiagnosticSignHint",  text = "" },
---     { name = "DiagnosticSignInfo",  text = "" },
--- }
---
--- for _, sign in ipairs(signs) do
---     vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
--- end
 
 vim.diagnostic.config({
     signs = {
