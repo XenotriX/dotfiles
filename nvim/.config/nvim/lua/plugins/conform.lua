a0c415141d1e38296e4794524ca677d9169e4e4c
Tibo Clausen <tibo.clausen@gmail.com>
2025-11-06 20:12:30 +0100

    feat(nvim): setup lua LSP and formatting

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -4,6 +4,7 @@ return {
         formatters_by_ft = {
             python = { 'isort', 'black' },
             cpp = { 'clang_format' },
+            lua = { 'stylua' },
         },
         default_format_opts = {
             lsp_format = "fallback",
