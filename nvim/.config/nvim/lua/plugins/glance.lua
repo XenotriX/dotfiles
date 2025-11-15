79f8fdff8baae6750dfcdb7ad69208ed38f4f214
Tibo Clausen <tibo.clausen@gmail.com>
2025-11-15 15:42:16 +0100

    feat(nvim): add glance
    Adds the glance plugin which provides a more fancy way to glance into
    symbol definitions, implementation, etc. Mapped to <leader>l{d,r,t,i}

---
+++ nvim/.config/nvim/lua/plugins/glance.lua
@@ -0,0 +1,10 @@
+return {
+    "dnlhc/glance.nvim",
+    cmd = "Glance",
+    keys = {
+        { "<leader>ld", "<cmd>Glance definitions<cr>", desc = "Glance Definitions" },
+        { "<leader>lr", "<cmd>Glance references<cr>", desc = "Glance References" },
+        { "<leader>lt", "<cmd>Glance type_definitions<cr>", desc = "Glance Type Definitions" },
+        { "<leader>li", "<cmd>Glance implementations<cr>", desc = "Glance Implementations" },
+    },
+}
