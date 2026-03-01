4496f499c56cde68c5a9f3d9794b2a97c82239bc
Tibo Clausen <tibo.clausen@gmail.com>
2026-03-01 14:46:42 +0100

    feat(zsh): brighten git branch color in prompt

--- zsh/.zshrc.d/40-prompt.zsh
+++ zsh/.zshrc.d/40-prompt.zsh
@@ -10,5 +10,5 @@ autoload -Uz vcs_info
 precmd_vcs_info() { vcs_info }
 precmd_functions+=( precmd_vcs_info )
 setopt prompt_subst
-zstyle ':vcs_info:git:*' formats '%F{240}(%b)%f'
+zstyle ':vcs_info:git:*' formats ' %F{#C044FF}(%b)%f'
 zstyle ':vcs_info:*' enable git
